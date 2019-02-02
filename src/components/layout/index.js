8c476477a7f690f09bf5e2e550e4fd3a25a624fa
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-02-02 23:52:34 +0530

    chore(package): Adds and configures ant design

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,14 +1,100 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Layout, Menu, Icon } from 'antd'
+import styled from 'styled-components'
 
-class Layout extends Component {
+const { Header, Content, Footer, Sider } = Layout
+const SubMenu = Menu.SubMenu
+
+const StyledLogo = styled.div`
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+`
+
+class LayoutComponent extends Component {
+  state = {
+    collapsed: false,
+  }
+
+  onCollapse = collapsed => {
+    console.log(collapsed)
+    this.setState({ collapsed })
+  }
+
   render() {
-    return <div>{this.props.children}</div>
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <StyledLogo />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '24px' }}>
+            <div
+              style={{
+                padding: 24,
+                background: '#fff',
+                minHeight: 360,
+              }}
+            >
+              {this.props.children}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    )
   }
 }
 
-Layout.propTypes = {
+LayoutComponent.propTypes = {
   children: PropTypes.node,
 }
 
-export default Layout
+export default LayoutComponent
