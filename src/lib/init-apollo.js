adbbb5134cb9fa0ca88c822f347e9ae4fd477176
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-04-03 01:26:12 +0530

    feat(app): Configures subscriptions

--- src/lib/init-apollo.js
+++ src/lib/init-apollo.js
@@ -2,6 +2,9 @@ import { ApolloClient } from 'apollo-boost'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import fetch from 'isomorphic-unfetch'
+import { WebSocketLink } from 'apollo-link-ws'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
 
 let apolloClient = null
 
@@ -12,17 +15,40 @@ if (!process.browser) {
 
 function create(initialState) {
   // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
+  // Create an http link
+  const httpLink = new HttpLink({
+    uri: 'http://localhost:8080/v1alpha1/graphql', // Server URL (must be absolute)
+    credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+  })
+
+  // Create a WebSocket link
+  const wsLink = process.browser
+    ? new WebSocketLink({
+        // if you instantiate in the server, the error will be thrown
+        uri: 'ws://localhost:8080/v1alpha1/graphql', // Server URL (must be absolute)
+        options: {
+          reconnect: true,
+        },
+      })
+    : null
+
   return new ApolloClient({
     connectToDevTools: process.browser,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
-    link: new HttpLink({
-      uri: 'http://localhost:8080/v1alpha1/graphql', // Server URL (must be absolute)
-      credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
-      headers: {
-        Authorization:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjanJvZHBjajdhamhzMGE4NWEybjQ2ODhxIiwiaWF0IjoxNTQ5MTY2NDQ2fQ.fwFch5w6LOU1iCNIuKceQ4dYoYv5Z49AOYCkf6OV4zI',
-      },
-    }),
+    link: process.browser
+      ? split(
+          //only create the split in the browser
+          // split based on operation type
+          ({ query }) => {
+            const { kind, operation } = getMainDefinition(query)
+            return (
+              kind === 'OperationDefinition' && operation === 'subscription'
+            )
+          },
+          wsLink,
+          httpLink
+        )
+      : httpLink,
     cache: new InMemoryCache().restore(initialState || {}),
   })
 }
