ff0e440be5cc286175b1c73d226a84354faa434c
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-06-05 00:38:04 +0530

    feat(app): Changes pages to typescript

--- pages/projects/new.tsx
+++ pages/projects/new.tsx
@@ -1,6 +1,5 @@
 import { withRouter } from 'next/router'
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import withApollo from '../../lib/with-apollo'
 import withLayout from '../../lib/with-layout'
@@ -10,10 +9,6 @@ class New extends Component {
   render() {
     return <ProjectNew />
   }
-}
-
-New.propTypes = {
-  url: PropTypes.object,
 }
 
 export default withRouter(withApollo(withLayout(New)))
