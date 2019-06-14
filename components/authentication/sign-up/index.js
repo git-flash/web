089133ef358e0f08d540bce66715ba67acceffc3
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-06-14 16:15:24 +0530

    feat(app): Shows loader while signing up

--- components/authentication/sign-up/index.js
+++ components/authentication/sign-up/index.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
+
 import { Form, Button, Input, Card } from 'antd'
 import Router from 'next/router'
 
-class SignUp extends Component {
-  handleSubmit = () => {
-    this.props.form.validateFields(async (err, values) => {
+const SignUp = props => {
+  const [isLoading, setIsLoading] = useState(false)
+  const { getFieldDecorator } = props.form
+  const handleSubmit = () => {
+    props.form.validateFields(async (err, values) => {
       if (!err) {
+        setIsLoading(true)
+
         try {
           const response = await fetch(
             'http://localhost:1337/auth/local/register',
@@ -30,82 +35,79 @@ class SignUp extends Component {
 
           Router.push('/projects')
 
+          setIsLoading(false)
+
           window.location.reload()
         } catch (error) {
+          setIsLoading(false)
+
           console.error(error)
         }
       }
     })
   }
 
-  render() {
-    const { getFieldDecorator } = this.props.form
-
-    return (
-      <div className="mt-4">
-        <Form layout="vertical" onSubmit={this.handleSubmit}>
-          <Form.Item label="Username">
-            {getFieldDecorator('username', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please enter username!',
-                },
-              ],
-              initialValue: 'admin@admin.com',
-            })(
-              <Input
-                placeholder="Please enter username"
-                size="large"
-                type="username"
-              />
-            )}
-          </Form.Item>
-          <Form.Item label="Email">
-            {getFieldDecorator('email', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please enter email!',
-                },
-              ],
-              initialValue: 'admin@admin.com',
-            })(
-              <Input
-                placeholder="Please enter email"
-                size="large"
-                type="email"
-              />
-            )}
-          </Form.Item>
-          <Form.Item label="Password">
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please enter password!' }],
-              initialValue: 'password',
-            })(
-              <Input
-                placeholder="Please enter password"
-                size="large"
-                type="password"
-              />
-            )}
-          </Form.Item>
-        </Form>
-        <div className="flex justify-end mt-12">
-          <Button
-            type="primary"
-            htmlType="submit"
-            onClick={this.handleSubmit}
-            size="large"
-            icon="check-circle"
-            block
-          >
-            Sign Up
-          </Button>
-        </div>
+  return (
+    <div className="mt-4">
+      <Form layout="vertical" onSubmit={handleSubmit}>
+        <Form.Item label="Username">
+          {getFieldDecorator('username', {
+            rules: [
+              {
+                required: true,
+                message: 'Please enter username!',
+              },
+            ],
+            initialValue: 'admin@admin.com',
+          })(
+            <Input
+              placeholder="Please enter username"
+              size="large"
+              type="username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item label="Email">
+          {getFieldDecorator('email', {
+            rules: [
+              {
+                required: true,
+                message: 'Please enter email!',
+              },
+            ],
+            initialValue: 'admin@admin.com',
+          })(
+            <Input placeholder="Please enter email" size="large" type="email" />
+          )}
+        </Form.Item>
+        <Form.Item label="Password">
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please enter password!' }],
+            initialValue: 'password',
+          })(
+            <Input
+              placeholder="Please enter password"
+              size="large"
+              type="password"
+            />
+          )}
+        </Form.Item>
+      </Form>
+      <div className="flex justify-end mt-12">
+        <Button
+          type="primary"
+          htmlType="submit"
+          onClick={handleSubmit}
+          size="large"
+          icon="check-circle"
+          block
+          loading={isLoading}
+        >
+          Sign Up
+        </Button>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Form.create()(SignUp)
