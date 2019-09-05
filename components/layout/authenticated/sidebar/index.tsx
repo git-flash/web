45f9b53858d08906c6a2f75c0a128f85472d4f1d
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-09-05 23:18:13 +0530

    feat(app): Adds active class to sidebar menu item

--- components/layout/authenticated/sidebar/index.tsx
+++ components/layout/authenticated/sidebar/index.tsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import { Layout } from 'antd'
 import { Menu, Icon } from 'antd'
-import Router from 'next/router'
+import { withRouter } from 'next/router'
 
-const AuthenticatedSidebar = () => {
+const AuthenticatedSidebar = (props: any) => {
   const [isSidebarCollapsed, collapseSidebar] = useState(false)
   const { Sider } = Layout
 
   useEffect(() => {
     window.innerWidth < 1024 && collapseSidebar(true)
   })
+
+  const getClassName = (key: string) => {
+    if (props.router.asPath === key) {
+      return 'ant-menu-item-selected'
+    }
+
+    return ''
+  }
 
   return (
     <Sider
@@ -22,29 +30,32 @@ const AuthenticatedSidebar = () => {
         <Menu
           theme="dark"
           mode="inline"
-          onClick={({ key }) => Router.push(key)}
+          onClick={({ key }) => props.router.push(key)}
         >
-          <Menu.Item key="/dashboard">
+          <Menu.Item key="/dashboard" className={getClassName('/dashboard')}>
             <Icon type="appstore" />
             <span className="font-bold">Dashboard</span>
           </Menu.Item>
-          <Menu.Item key="/sites">
+          <Menu.Item key="/sites" className={getClassName('/sites')}>
             <Icon type="project" />
             <span className="font-bold">Sites</span>
           </Menu.Item>
-          <Menu.Item key="/users">
+          <Menu.Item key="/users" className={getClassName('/users')}>
             <Icon type="user" />
             <span className="font-bold">Users</span>
           </Menu.Item>
-          <Menu.Item key="/teams">
+          <Menu.Item key="/teams" className={getClassName('/teams')}>
             <Icon type="team" />
             <span className="font-bold">Teams</span>
           </Menu.Item>
-          <Menu.Item key="/information">
+          <Menu.Item
+            key="/information"
+            className={getClassName('/information')}
+          >
             <Icon type="info-circle" />
             <span className="font-bold">Information</span>
           </Menu.Item>
-          <Menu.Item key="/audits">
+          <Menu.Item key="/audits" className={getClassName('/audits')}>
             <Icon type="audit" />
             <span className="font-bold">Audits</span>
           </Menu.Item>
@@ -54,4 +65,4 @@ const AuthenticatedSidebar = () => {
   )
 }
 
-export default AuthenticatedSidebar
+export default withRouter(AuthenticatedSidebar)
