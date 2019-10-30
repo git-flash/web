5c46bea6c01ac27ed5c05cbaf1ca85f831878771
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-10-30 19:27:44 +0530

    feat(app): Adds search bar for searching sites

--- components/layout/authenticated/header.js
+++ components/layout/authenticated/header.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { Layout, Menu, Dropdown, Icon, Button } from 'antd'
+import { Layout, Menu, Dropdown, Icon, Button, AutoComplete } from 'antd'
 import Router from 'next/router'
 import Link from 'next/link'
 import Gravatar from 'react-gravatar'
+import { graphql, withApollo, useQuery } from 'react-apollo'
+import gql from 'graphql-tag'
+import { withRouter } from 'next/router'
 
 import Logo from '../../../static/images/logo.svg'
 
-const { Header } = Layout
+const searchSiteQuery = gql`
+  query($query: String) {
+    site(where: { name: { _ilike: $query } }) {
+      id
+      name
+    }
+  }
+`
 
-const AuthenticatedLayoutHeader = ({ email }) => {
+const AuthenticatedLayoutHeader = ({ email, client, router }) => {
+  const { Header } = Layout
+  const { Option } = AutoComplete
+  const [queryResults, setQueryResults] = useState([])
+
   const handleSignOut = () => {
     document.cookie = `userId=`
     document.cookie = `email=`
@@ -18,11 +32,34 @@ const AuthenticatedLayoutHeader = ({ email }) => {
     Router.push('/authentication')
   }
 
+  const handleSearch = async query => {
+    const res = await client.query({
+      query: searchSiteQuery,
+      variables: {
+        query: `%${query}%`,
+      },
+    })
+
+    const matchedSites =
+      res &&
+      res.data &&
+      res.data.site.map(s => ({
+        id: s.id,
+        name: s.name,
+      }))
+
+    if (!query) {
+      setQueryResults([])
+    } else {
+      setQueryResults(matchedSites)
+    }
+  }
+
   return (
     <Header style={{ background: '#fff', padding: 0 }}>
       <div className="flex justify-between h-full border border-solid border-gray-300">
         <div className="mx-4 flex">
-          <Link href={`/`} as={`/`}>
+          <Link href={`/dashboard`} as={`/dashboard`}>
             <a>
               <img
                 src={Logo}
@@ -33,6 +70,25 @@ const AuthenticatedLayoutHeader = ({ email }) => {
               />
             </a>
           </Link>
+          <div className="px-4">
+            <AutoComplete
+              dataSource={queryResults}
+              style={{ width: 500 }}
+              size="large"
+              onSelect={siteId => {
+                router.push(`/sites/[siteId]?id=${siteId}`, `/sites/${siteId}`)
+              }}
+              onSearch={value => handleSearch(value)}
+              placeholder="Search for a site"
+              optionLabelProp="name"
+            >
+              {queryResults.map(site => (
+                <Option key={site.id} value={site.id}>
+                  {site.name}
+                </Option>
+              ))}
+            </AutoComplete>
+          </div>
         </div>
         <div className="mx-4 flex">
           <div className="px-4">
@@ -69,4 +125,4 @@ AuthenticatedLayoutHeader.propTypes = {
   email: PropTypes.string,
 }
 
-export default AuthenticatedLayoutHeader
+export default withRouter(withApollo(AuthenticatedLayoutHeader))
